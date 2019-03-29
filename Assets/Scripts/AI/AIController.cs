094dbf22f7f0dcf1ca30d6703a97fca4739db6ad
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-03-29 13:32:59 -0400

    Added AIController

---
+++ Assets/Scripts/AI/AIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+    public AI.BehaviorTree myTree;
+    public AI.Blackboard localBlackboard;// = new AI.Blackboard();
+    public int treeUpdateInterval = 64;
+
+    private int _treeTicks = 0; 
+
+    protected virtual void FixedUpdate()
+    {
+        if(myTree && (localBlackboard != null))
+        {
+            if (_treeTicks <= 0)
+            {
+                myTree.ProcessTree(localBlackboard);
+                _treeTicks = treeUpdateInterval;
+            }
+            _treeTicks--;
+        }
+    }
+}
