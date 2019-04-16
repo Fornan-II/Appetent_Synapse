7f96b90421a23273ea9a1b451567016ed39b836f
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-04-16 14:11:17 -0400

    Started range weapon\

---
+++ Assets/Scripts/Items/Weapons/RangedWeapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedWeapon : Weapon
+{
+    public DamagePacket damage = new DamagePacket(7, 5.0f);
+    public float maxRange = 64.0f;
+    public LayerMask hittable;
+
+    protected Animator _anim;
+
+    protected virtual void Start()
+    {
+        _anim = gameObject.GetComponent<Animator>();
+    }
+
+    public override bool DoAttack(GameObject target, Pawn user)
+    {
+        //Just basic hitscan
+        if(_anim)
+        {
+            _anim.SetTrigger("attack");
+        }
+        //Physics.Raycast()
+    }
+}
