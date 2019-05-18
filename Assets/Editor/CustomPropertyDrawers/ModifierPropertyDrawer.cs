096e9ac8a06d06b9eaae8601f588959e0bdecca6
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-05-17 21:50:46 -0400

    Modifier Type & Property Drawer
    Basically a float that can hold a list of floats to multiply itself with.

---
+++ Assets/Editor/CustomPropertyDrawers/ModifierPropertyDrawer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(Modifier))]
+public class ModifierFloatPropertyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        //base.OnGUI(position, property, label);
+
+        EditorGUI.BeginProperty(position, label, property);
+
+        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+        int indent = EditorGUI.indentLevel;
+        EditorGUI.indentLevel = 0;
+
+        Rect baseValueRect = new Rect(position.x, position.y, position.width / 2.0f - 2.5f, position.height);
+        Rect valueRect = new Rect(position.x + baseValueRect.width + 5, position.y, position.width / 2.0f - 2.5f, position.height);
+
+        SerializedProperty baseValue = property.FindPropertyRelative("_baseValue");
+        EditorGUI.PropertyField(baseValueRect, baseValue, GUIContent.none);
+        GUI.enabled = false;
+        EditorGUI.PropertyField(valueRect, property.FindPropertyRelative("_value"), GUIContent.none);
+        GUI.enabled = true;
+
+        EditorGUI.indentLevel = indent;
+
+        EditorGUI.EndProperty();
+    }
+}
---
+++ Assets/Scripts/Modifier.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Modifier
+{
+    [SerializeField]protected float _baseValue;
+    public float BaseValue
+    {
+        get
+        {
+            return _baseValue;
+        }
+        set
+        {
+            _baseValue = value;
+            RefreshCachedValue();
+        }
+    }
+
+    [SerializeField]protected float _value;
+    public float Value
+    {
+        get
+        {
+            return _value;
+        }
+    }
+
+    protected Dictionary<string, float> _modifiers;
+
+    public Modifier(float baseValue)
+    {
+        _baseValue = baseValue;
+        _modifiers = new Dictionary<string, float>();
+        RefreshCachedValue();
+    }
+
+    public Modifier(float baseValue, Dictionary<string, float> modifiers)
+    {
+        _baseValue = baseValue;
+        _modifiers = modifiers;
+        RefreshCachedValue();
+    }
+
+    public virtual void SetModifier(string key, float value)
+    {
+        if (_modifiers == null)
+        {
+            _modifiers = new Dictionary<string, float>();
+        }
+
+        if (_modifiers.ContainsKey(key))
+        {
+            if(_modifiers[key] != value)
+            {
+                _modifiers[key] = value;
+                RefreshCachedValue();
+            }
+        }
+        else
+        {
+            _modifiers.Add(key, value);
+            RefreshCachedValue();
+        }
+    }
+
+    public virtual void RemoveModifier(string key)
+    {
+        if (_modifiers == null) { return; }
+
+        if (_modifiers.ContainsKey(key))
+        {
+            _modifiers.Remove(key);
+            RefreshCachedValue();
+        }
+    }
+
+    public virtual void ClearModifiers()
+    {
+        if (_modifiers == null) { return; }
+
+        if (_modifiers.Count > 0)
+        {
+            _modifiers.Clear();
+            RefreshCachedValue();
+        }
+    }
+
+    protected virtual void RefreshCachedValue()
+    {
+        _value = _baseValue;
+
+        if (_modifiers != null)
+        {
+            foreach (float modifier in _modifiers.Values)
+            {
+                _value *= modifier;
+            }
+        }
+    }
+}
