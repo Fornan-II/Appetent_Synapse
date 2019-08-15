17692aee85557df33413cd12e1f31763a314d083
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-08-15 19:12:53 -0400

    Conference room work

---
+++ Assets/Scripts/Interaction/UniqueInteractions/StartAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAnimation : MonoBehaviour
+{
+    public string AnimationVariableName = "InteractTrigger";
+    public Animator TargetAnimator;
+
+    public void TriggerAnimation(Pawn source)
+    {
+        if(TargetAnimator)
+        {
+            TargetAnimator.SetTrigger(AnimationVariableName);
+        }
+    }
+}
