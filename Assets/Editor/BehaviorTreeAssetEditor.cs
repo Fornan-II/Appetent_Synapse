0b38ed5a72d02aff38a628f883eb3cd595ba8e5d
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-12-10 00:47:25 -0500

    Editor/Drawers WIP

---
+++ Assets/Editor/BehaviorTreeAssetEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+using AI;
+
+[CustomEditor(typeof(BehaviorTreeAsset))]
+public class BehaviorTreeAssetEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+}
--- Assets/Editor/CustomPropertyDrawers/AI/NodePropertyDrawer.cs
+++ Assets/Editor/CustomPropertyDrawers/AI/NodePropertyDrawer.cs
@@ -1,4 +1,6 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace AI.Editor
@@ -6,27 +8,76 @@ namespace AI.Editor
     [CustomPropertyDrawer(typeof(BehaviorTree.Node))]
     public class NodePropertyDrawer : PropertyDrawer
     {
-        private int selected = 0;
+        //    private int selected = 0;
 
-        private static string[] SubTypeNames = new string[] { "Root", "Selector", "Sequence", "Leaf" };
-        private static PropertyDrawer[] SubTypeDrawers = new PropertyDrawer[]
+        //    //Idea of Dictionary: https://answers.unity.com/questions/1499762/using-custom-property-drawers-with-polymorphism.html
+        //    private static string[] SubTypeNames = new string[] { "Unselected", "Selector", "Sequence", "Leaf" };
+        //    private static Dictionary<Type, int> SubTypeIndeces = new Dictionary<Type, int>
+        //    {
+        //        { typeof(BehaviorTree.Selector), 1 },
+        //        { typeof(BehaviorTree.Sequence), 2 },
+        //        { typeof(BehaviorTree.Leaf), 3 }
+        //    };
+
+        //    private static PropertyDrawer[] SubTypeDrawers = new PropertyDrawer[]
+        //    {
+        //        null,
+        //        new SelectorPropertyDrawer(),
+        //        new SequencePropertyDrawer(),
+        //        new LeafPropertyDrawer()
+        //    };
+
+        static Dictionary<Type, PropertyDrawer> SubTypeDrawers = new Dictionary<Type, PropertyDrawer>()
         {
-            //new RootPropertyDrawer(),
-            //new SelectorPropertyDrawer(),
-            //new SequencePropertyDrawer(),
-            //new LeafPropertyDrawer()
+            { typeof(BehaviorTree.Root), new RootPropertyDrawer() },
+            { typeof(BehaviorTree.Selector), new SelectorPropertyDrawer() },
+            { typeof(BehaviorTree.Sequence), new SequencePropertyDrawer() },
+            { typeof(BehaviorTree.Leaf), new LeafPropertyDrawer() }
         };
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EditorGUI.BeginProperty(position, label, property);
 
-            System.Type t = property.serializedObject.targetObject.GetType();
-            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), new GUIContent(t.ToString()));
-            //EditorGUI.PropertyField(position, new UnityEditor.SerializedObject());
-            selected = EditorGUI.Popup(position, 0, SubTypeNames);
+            UnityEngine.Object obj = property.objectReferenceValue;
+            if(obj)
+            {
+                SubTypeDrawers[obj.GetType()].OnGUI(position, property, label);
+            }
+            else
+            {
+                EditorGUI.LabelField(position, "NULL");
+            }
 
-            //SubTypeDrawers[selected].OnGUI(position, property, label);
+    //        if (property.serializedObject.targetObject == null)
+    //        {
+    //            selected = 0;
+    //        }
+    //        else
+    //        {
+    //            Type targetType = property.serializedObject.targetObject.GetType();
+    //            if(SubTypeIndeces.ContainsKey(targetType))
+    //            {
+    //                selected = SubTypeIndeces[targetType];
+
+    //            }
+    //        }
+    //        //position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), new GUIContent("Node"));
+    //        //EditorGUI.PropertyField(position, new UnityEditor.SerializedObject());
+    //        int oldSelected = selected;
+    //        selected = EditorGUI.Popup(position, selected, SubTypeNames);
+
+    //        //SerializedObject serializedObject = new SerializedObject(property.objectReferenceValue as Monkey);
+
+    //        position.y += position.height;
+    //        //EditorGUI.LabelField(position, property.objectReferenceValue.ToString());
+
+    //        if (oldSelected != selected)
+    //        {
+                
+    //        }
+
+    //        //SubTypeDrawers[selected].OnGUI(position, property, label);
 
             EditorGUI.EndProperty();
         }
